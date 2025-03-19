deaf42d326d89ecaaacb17f1c8bc066195ad0791
MqxS <maxgoog06@outlook.com>
2025-03-19 17:42:59 -0400

    goated key binds

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -19,13 +19,13 @@ public class Constants {
     public static final String DASH_SELECTED_BRANCH = "SelectedBranch";
 
     public static Map<Integer, Branch> KEYBOARD_TO_BRANCH = Map.of(
-            53, Branch.LEFT_L4,
-            3639, Branch.RIGHT_L4,
-            9, Branch.LEFT_L3,
-            10, Branch.RIGHT_L3,
-            6, Branch.LEFT_L2,
+            5, Branch.LEFT_L4,
+            9, Branch.RIGHT_L4,
+            4, Branch.LEFT_L3,
+            8, Branch.RIGHT_L3,
+            3, Branch.LEFT_L2,
             7, Branch.RIGHT_L2,
-            3, Branch.LEFT_L1,
-            4, Branch.RIGHT_L1
+            2, Branch.LEFT_L1,
+            6, Branch.RIGHT_L1
     );
 }
